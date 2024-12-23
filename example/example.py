574855d24a87235982d03de7c64bf6c1cd65b9b8
Braulio Otavalo <56369660+beotavalo@users.noreply.github.com>
2024-12-23 01:09:34 +0000

    feat:completes uv and ruff example

---
+++ example/example.py
@@ -0,0 +1,4 @@
+"""A script test for using uv and ruff."""
+import requests
+
+print(requests.get("https://astral.sh"))
---
+++ example/hello.py
@@ -0,0 +1,8 @@
+"""A script test for using uv and ruff."""
+def main():
+    """Print a greeting message."""
+    print("Hello from example!")
+
+
+if __name__ == "__main__":
+    main()
